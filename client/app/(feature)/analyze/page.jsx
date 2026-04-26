b194341dcae3331ea9cde3e0fe76c80f1b2d9dbe
Divyansh Rajput <rajputkrishuuu@gmail.com>
2026-04-26 19:22:12 +0530

    added protected routes

--- client/app/(feature)/analyze/page.jsx
+++ client/app/(feature)/analyze/page.jsx
@@ -1,11 +1,23 @@
+"use client";
 import React from 'react'
 import NavBar from '@/components/NavBar.jsx'
+import { useRouter } from 'next/navigation'
+import { useAuth } from '../../../hooks/useAuth'
+
 
 const page = () => {
+  const { user } = useAuth()
+  const router = useRouter()
+
+  if (!user) {
+    return <div>Please log in to access this page. <button className='bg-white py-2 px-4 text-black rounded-3xl hover:scale-90' onClick={() => router.push('/login')}>Go to Login</button></div>
+  }
+
   return (
     <div>
       <NavBar />
-      analyse page
+      <h1>Analysis Page</h1>
+      <p>Welcome, {user.name}!</p>
     </div>
   )
 }
--- client/context/auth.context.jsx
+++ client/context/auth.context.jsx
@@ -1,5 +1,7 @@
 "use client";
 import { createContext, useState } from "react";
+import { getme } from "@/services/auth.api.js";
+import { useEffect } from "react";
 
 export const authContext = createContext();
 
@@ -7,6 +9,22 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(false);
     
+    useEffect(() => {
+        const fetchUser = async () => {
+            setLoading(true);
+            try {
+                const userData = await getme();
+                setUser(userData);
+            } catch (error) {
+                console.error("Failed to fetch user data:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
     return (
         <authContext.Provider value={{ user, setUser, loading, setLoading }}>
             {children}
